508c1a874a5ec0e5ec12ae8858b0f56bcc0ad88a
rohith-rongala <118845129+rohith-rongala@users.noreply.github.com>
2024-05-06 10:55:52 +0530

    CloudWatch Rule added as trigger - syndicate

--- task07/jsrc/main/java/com/task07/UuidGenerator.java
+++ task07/jsrc/main/java/com/task07/UuidGenerator.java
@@ -2,25 +2,56 @@ package com.task07;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.google.gson.Gson;
+import com.syndicate.deployment.annotations.events.RuleEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @LambdaHandler(lambdaName = "uuid_generator",
 	roleName = "uuid_generator-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
+	isPublishVersion = false,
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class UuidGenerator implements RequestHandler<Object, Map<String, Object>> {
+@RuleEventSource(targetRule = "uuid_trigger")
+public class UuidGenerator implements RequestHandler<Object, String> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+	private AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
+
+	public String handleRequest(Object request, Context context) {
+		String bucket = "uuid-storage"; // replace with your bucket name
+		String key = LocalDateTime.now().toString() + ".json";
+
+		// Generate 10 UUIDs
+		Map<String, Object> data = new HashMap<>();
+		data.put("ids", IntStream.range(0, 10)
+				.mapToObj(i -> UUID.randomUUID().toString())
+				.collect(Collectors.toList()));
+
+		Gson gson = new Gson();
+
+		// Upload to S3
+		InputStream stream = new ByteArrayInputStream(gson.toJson(data).getBytes(StandardCharsets.UTF_8));
+		ObjectMetadata metadata = new ObjectMetadata();
+		metadata.setContentLength(gson.toJson(data).length());
+
+		context.getLogger().log("Writing to: " + bucket + "/" + key);
+		s3.putObject(new PutObjectRequest(bucket, key, stream, metadata));
+		return "UUID written to S3 bucket";
 	}
 }
+
