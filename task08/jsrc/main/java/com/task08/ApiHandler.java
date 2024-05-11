24bcccaa22348f244bf14a48a811c494e87db8ed
rohith-rongala <118845129+rohith-rongala@users.noreply.github.com>
2024-05-11 16:41:18 +0530

    Imported from layers and invoked method

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -11,13 +11,20 @@ import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.io.File;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @LambdaHandler(lambdaName = "api_handler",
 	roleName = "api_handler-role",
-	isPublishVersion = false,
+    isPublishVersion = true,
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED,
 		layers = {"sdk_layer"},
 		runtime = DeploymentRuntime.JAVA11
@@ -32,10 +39,48 @@ import java.util.concurrent.CompletableFuture;
 public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
-		OpenMeteoClient openMeteo = new OpenMeteoClient();
-		CompletableFuture<String> weather = openMeteo.getWeather();
+		//OpenMeteoClient openMeteo = new OpenMeteoClient();
+		//CompletableFuture<String> weather = openMeteo.getWeather();
 
-		Map<String, Object> resultMap = new HashMap<String, Object>();
+		File file = new File("/opt/java/lib/task08-1.0.0.jar");
+        URL url = null;
+        try {
+            url = file.toURI().toURL();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        URL[] urls = new URL[]{url};
+		ClassLoader cl = new URLClassLoader(urls);
+        Class cls = null;
+        try {
+            cls = cl.loadClass("com.task08.OpenMeteoClient");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        Object openMeteoObj = null;
+        try {
+            openMeteoObj = cls.newInstance();
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        Method method = null;
+        try {
+            method = cls.getDeclaredMethod("getWeather");
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+        CompletableFuture<String> weather = null;
+        try {
+            weather = (CompletableFuture<String>) method.invoke(openMeteoObj , new Object[]{});
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap.put("statusCode", 200);
 		resultMap.put("body", weather.join());
 		return resultMap;
