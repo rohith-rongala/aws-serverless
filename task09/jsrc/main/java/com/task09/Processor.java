5b74b58438f5c64fbb8eccf70d647fa651f34b0c
rohith-rongala <118845129+rohith-rongala@users.noreply.github.com>
2024-05-12 22:19:10 +0530

    latitude and longitude hardcoded

--- task09/jsrc/main/java/com/task09/Processor.java
+++ task09/jsrc/main/java/com/task09/Processor.java
@@ -44,7 +44,7 @@ public class Processor implements RequestHandler<Object, String> {
 		try {
 			// Step 1: Call the weather API
 			HttpRequest request = HttpRequest.newBuilder()
-					.uri(URI.create("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"))
+					.uri(URI.create("https://api.open-meteo.com/v1/forecast?latitude=50.4375&longitude=30.5&hourly=temperature_2m"))
 					.build();
 			String responseBody = httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body();
 
